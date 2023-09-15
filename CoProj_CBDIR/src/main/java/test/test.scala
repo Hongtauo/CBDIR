5474242223fad4c203f63000b256c1c32d8e517e
Hongtauo <sunhongtao202103@163.com>
2023-09-15 19:40:54 +0800

    测试脚本正常运行，能够读取hadoop集群中的mealrating数据

---
+++ CoProj_CBDIR/src/main/java/test/test.scala
@@ -0,0 +1,26 @@
+package test
+import org.apache.spark.sql.SparkSession
+
+object test {
+  def main(args:Array[String]):Unit={
+    val spark = SparkSession.builder()
+      .appName("test")
+      .master("local[*]")
+      .enableHiveSupport()
+      .getOrCreate()
+
+    spark.sparkContext.setLogLevel("WARN")
+
+    // 读取HDFS上的结构化数据
+    val mealrating = spark.read.json("hdfs://master:8020/CBDIRDatas/MealRatings_201705_201706.json")
+    mealrating.show()
+
+    //  切换使用的数据库
+    spark.catalog.setCurrentDatabase("cbdir")
+
+    //  修改数据库CBDIR的权限的指令“hadoop fs -chmod 777 /user/hive/warehouse/cbdir.db”
+    mealrating.write.mode("overwrite").saveAsTable("MealRating")
+
+
+  }
+}
