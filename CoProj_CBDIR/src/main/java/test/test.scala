dc18d46c01566cb852d45ea11955694a9d5dbe0e
Hongtauo <sunhongtao202103@163.com>
2023-09-16 11:08:49 +0800

    工程文件中test类完成了在Hive的CBDIR数据库创建并写入'MealRating'、'meal_list'两张表，在spark可以直接读取表中数据进行处理

--- CoProj_CBDIR/src/main/java/test/test.scala
+++ CoProj_CBDIR/src/main/java/test/test.scala
@@ -1,5 +1,7 @@
 package test
 import org.apache.spark.sql.SparkSession
+import java.util.Properties
+
 
 object test {
   def main(args:Array[String]):Unit={
@@ -18,7 +20,7 @@ object test {
     //  切换使用的数据库
     spark.catalog.setCurrentDatabase("cbdir")
 
-    //  修改数据库CBDIR的权限的指令“hadoop fs -chmod 777 /user/hive/warehouse/cbdir.db”
+    //  修改数据库CBDIR的权限的指令“hadoop fs -chmod 777 /user/hive/warehouse/cbdir.db”，将DataFrame文件同步导出到Hive数据库CBDIR下
     mealrating.write.mode("overwrite").saveAsTable("MealRating")
 
     //  初步分析
@@ -29,7 +31,18 @@ object test {
 
     mealrating.groupBy("MealID","Review").count().show()
 
+    //  读取外部数据库sql中的文件，需要在IDEA中，从项目结构中导入mysql的驱动包.jar，该驱动包也在工程文件的Configs下
+    val pro = new Properties()
+    pro.put("driver", "com.mysql.cj.jdbc.Driver")
+    pro.put("user", "root")
+    pro.put("password", "123456")
 
+    val meal_list = spark.read.jdbc("jdbc:mysql://master:3306","test.meal_list",pro)
+    meal_list.show()
+    meal_list.write.mode("overwrite").saveAsTable("meal_list")
+
+    // 至此，Hive的数据库CBDIR中存在两张表：1、MealRating 2.meal_list
+    
 
   }
 }
